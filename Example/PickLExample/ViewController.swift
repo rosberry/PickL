49175f7c4ccad94a5d166e9c61ef53a0a648d6ef
Nikita Ermolenko <nikita.ermolenko@rosberry.com>
2017-09-01 17:01:34 +0600

    Generate example for testing

---
+++ Example/PickLExample/ViewController.swift
@@ -0,0 +1,22 @@
+//
+//  ViewController.swift
+//  PickLExample
+//
+//  Created by Nikita Ermolenko on 01/09/2017.
+//  Copyright © 2017 Rosberry. All rights reserved.
+//
+
+import UIKit
+import PickL
+
+class ViewController: UIViewController {
+
+    let pickerView = UIPickerView()
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+}
+
